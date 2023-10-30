e8371c4fee628d1f3986c0710138f5dfb0a3a66c
dindin <dindin222007@gmail.com>
2023-10-30 13:51:37 +0800

    title, footer

--- src/__components__/footer/footer-fixed.js
+++ src/__components__/footer/footer-fixed.js
@@ -1,6 +1,6 @@
 export const FooterFixed = () => {
   return (
-    <div style={{ position: 'fixed', bottom: '50px', width: '100%' }} id='footer' className='app-footer mx-0 px-0 mb-100px'>
+    <div style={{ position: 'fixed', bottom: '25px', width: '80%' }} id='footer' className='app-footer mx-0 px-0 mb-100px'>
       &copy; 2022 Metropolitan Chess Center - Dindin All Rights Reserved
     </div>
   )
--- src/__components__/footer/footer.js
+++ src/__components__/footer/footer.js
@@ -1,7 +1,14 @@
 export const Footer = () => {
   return (
-    <div id='footer' className='app-footer mx-0 px-0 mb-100px'>
-      &copy; 2022 Metropolitan Chess Center - Dindin All Rights Reserved
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div id='footer' className='app-footer mx-0 px-0 mb-100px'>
+        &copy; 2022 Metropolitan Chess Center - Dindin All Rights Reserved
+      </div>
     </div>
   )
 }
