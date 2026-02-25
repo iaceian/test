ae4ec6d3b9fa63ba75b12254f5728f581002bed0
dindin222007 <dindin222007@gmail.com>
2026-02-25 13:11:39 +0800

    events 2026

--- src/__components__/footer/footer-fixed.js
+++ src/__components__/footer/footer-fixed.js
@@ -1,7 +1,7 @@
 export const FooterFixed = () => {
   return (
     <div style={{ position: 'fixed', bottom: '25px', width: '80%' }} id='footer' className='app-footer mx-0 px-0 mb-100px'>
-      &copy; 2022 Metropolitan Chess Center - Dindin All Rights Reserved
+      &copy; 2026 Metropolitan Chess Center - Dindin All Rights Reserved
     </div>
   )
 }
