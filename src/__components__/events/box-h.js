90c9d24d10d4c55fce1b542635d521db6e7ec854
dindin222007 <dindin222007@gmail.com>
2026-05-14 11:03:17 +0800

    Change May 16-17 register

--- src/__components__/events/box-h.js
+++ src/__components__/events/box-h.js
@@ -29,12 +29,12 @@ export const BoxH = () => {
                   </button>
                 </td>
                 <td>
+                  <StandardRegistrationForm />
+                </td>
+                <td>
                   <button type='button' className='btn btn-outline-info me-1 mb-1'>
                     <Link to='../assets/doc/register/Fide-rated Standard 1800 Below Tournament Registration Form.doc' target='_blank'>Register</Link>
                   </button>
-                </td>
-                <td>
-                  <StandardRegistrationForm />
                 </td>
                 <td>
                   <StandardRegistrationForm />
